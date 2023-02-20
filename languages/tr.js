776988186337fe6f10c162ec388b9a56c58cc1da
Nicat <59221034+Nicat-dcw@users.noreply.github.com>
2023-02-20 23:56:56 +0400

    Create tr.js

---
+++ languages/tr.js
@@ -0,0 +1,28 @@
+module.exports = {   
+    gw:{
+        "winners":"Kazanan(lar)",
+         "prize":"Ödül",
+         "ends":"Bitiş",
+         "button":"Katıl!",
+         "gwCreated":"Çekiliş Başarıyla Oluşturuldu.",
+         userEnteredGiveaway:"Çekilişe Katıldın!",
+         userLeftGiveaway:"Çekilişe Katılım hakkın silindi.!",
+         presenceReqRequired:"Bu Çekiliş **Durum** Şartı Gerektiriyor.",
+         requirementAdded:"**Şart** Başarıyla eklendi.",
+         chanceToWin:"Kazanma Şansın:"
+    },
+    help:{
+         title:"Yardım Menüsü",
+         versions:"Versiyon Listesi",
+         globalVersion:"Global Versiyon",
+         betaVersion:"Beta Versiyon",
+         menuTitle:"Yardım Menüsü",
+         gwLabel:"Çekiliş Komutları",
+         gwDescription:"Tüm Çekiliş kokutlarını gösterir."
+    },
+      commands:{
+        language:{
+            embedTitle:"Dil Menüsü"
+        }
+    
+ }
