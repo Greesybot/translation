3a58a3955d0b52a5ca2f93a982e789498b8b0482
Nicat <59221034+Nicat-dcw@users.noreply.github.com>
2023-02-20 23:55:29 +0400

    Create en.js

---
+++ languages/en.js
@@ -0,0 +1,28 @@
+module.exports = {   
+    gw:{
+        winners:"Winner(s)",
+         prize:"Prize",
+         "ends":"Ends",
+         "button":"Enter",
+         "gwCreated":"Giveaway Created Successfully.",
+         userEnteredGiveaway:"You Entered the giveaway!",
+         userLeftGiveaway:"Your Enterance removed!",
+         presenceReqRequired:"This Giveaway have **Presence Requirement**.",
+         requirementAdded:"**Requirement** added to the giveaway",
+         chanceToWin:"Your Win Rate:"
+    },
+    help:{
+         title:"Help Menu",
+         versions:"Version List",
+         globalVersion:"Global Version",
+         betaVersion:"Beta Version",
+         menuTitle:"Help Menu",
+         gwLabel:"Giveaway Commands",
+         gwDescription:"Shows All Giveaway Commands."
+    },
+    commands:{
+        language:{
+            embedTitle:"Language Menu"
+        }
+    }
+ }
