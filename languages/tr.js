09f703ca67bbd318313c17025b216d4c8b82cbe7
Nicat <59221034+Nicat-dcw@users.noreply.github.com>
2023-03-03 12:02:48 +0400

    Update tr.js

--- languages/tr.js
+++ languages/tr.js
@@ -8,11 +8,24 @@ module.exports = {
          userEnteredGiveaway:"Çekilişe Katıldın!",
          userLeftGiveaway:"Çekilişe Katılım hakkın silindi.!",
          presenceReqRequired:"Bu Çekiliş **Durum** Şartı Gerektiriyor.",
+         roleReqRequired:"Bu Çekiliş **Rol** Şartı Gerektiriyor.",
          requirementAdded:"**Şart** Başarıyla eklendi.",
-         chanceToWin:"Kazanma Şansın:"
+         chanceToWin:"Kazanma Şansın:",
+         notfound:"Böyle bir Çekiliş bulunamadı.",
+         giveawayDeletedSuccessfully:"**Çekiliş** başarıyla silindi.",
+     events:{
+         endedButNoneEntrans:"Yeterli katılım olmadığı için Çekiliş iptal edildi.",
+         ended:"Kazanan(lar): {{winners}}",
+         congrulations:"🎉 Tebrikler!",
+         mustHave:"Sahip olmak",
+         req:"Gereksinimler",
+         reroll:"Çekilişi Tekrarla"
+     }
     },
     help:{
          title:"Yardım Menüsü",
+         description:"Merhaba! Uzun zamandır bir güncelleme almıyordum, Benim Yeni Versiyonuma Merhaba Deyin!",
+         newsDescription:"Yeni Versiyon ile Çok şey değişecek, daha yeni sistemler daha iyi performans ve daha fazlası!!",
          versions:"Versiyon Listesi",
          globalVersion:"Global Versiyon",
          betaVersion:"Beta Versiyon",
@@ -21,8 +34,42 @@ module.exports = {
          gwDescription:"Tüm Çekiliş kokutlarını gösterir."
     },
       commands:{
+        noPermission:"> :x: Bu Komutu kullanmak için gerekli yetki(leri)niz yoktur.",  
         language:{
             embedTitle:"Dil Menüsü"
+        },
+         stats:{
+          /*Data Titles*/
+            globalData: "Global Veri",
+            greesyData:"Greesy Verileri",
+            pingData: "Gecikme Bilgileri",
+            versionData: "Versiyon Verileri",
+          /*Message Titles*/
+            serverSize:"Sunucu sayı",
+            usersSize:"Kullanıcı sayı",
+            databaseStatus:"Veritabanı Durumu",
+            greesyVersion:"Greesy Global Versiyonu",
+            totalGiveaways:"Toplam Başlatılmış Çekiliş sayısı",
+            wsPing:"WebSocket Gecikmesi",
+            databasePing:"Database Gecimesi",
+            nodejs:"NodeJS",
+            discordjs:"Discord.JS"
+
+        },
+       
+        invite:{
+            embedTitle:"Senin Davet Sayın",
+            description:"> 🌱 Sen **{{invite_count}}** Davete Sahipsin.\n\n**Davet Kodları**;\n> {{invite_codes}}",
+        top:{
+            boardMessage:"{{user}} • **{{count}}**"
         }
-    
+     
+           }
+      },
+      events:{
+        invite:{
+            joinMessage:"{{user}} Sunucuya Katıldı! Onu davet eden {{inviter}}. \n Davet Eden **{{total_invite_count}}** Davete Sahip Oldu."
+        }
+      }
+
  }
